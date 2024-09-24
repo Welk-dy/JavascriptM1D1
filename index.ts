14e94893472ea9955e3a5d213279521aab819cdf
Welk-dy <Fernaldysaputra@gmail.com>
2024-09-24 10:04:18 +0700

    commit

--- index.ts
+++ index.ts
@@ -1,69 +1,106 @@
-// Pizza shop app
-// that has a menu of pizzas
-// and a cart to add pizzas to
-// and a checkout to pay for pizzas
+/*
+  1. Write a code to find area of rectangle
+  Input: length = 5, width = 3
+  Output: 15
+*/
+const areaOfRectangle = (length: number, width: number): number => {
+  return length * width;
+};
+console.log(`Area of rectangle : ${areaOfRectangle(5, 3)}`);
 
-import { Cart, Menu, Order } from "./types/pizza";
+/*
+  2. Write a code to find diameter, circumference and area of a circle
+  Input: radius = 5
+  Output: diameter = 10, circumference = 31.4159, area = 78.539
+*/
+const circleProperties = (radius: number): void => {
+  const diameter = 2 * radius;
+  const circumference = 2 * Math.PI * radius;
+  const area = Math.PI * Math.pow(radius, 2);
 
-// public Menu initMenu()
-function initMenu(): Menu {
-  return {
-    pizzas: [
-      { name: "Margherita", price: 5 },
-      { name: "Fungi", price: 3 },
-      { name: "Pepperoni", price: 6 },
-      { name: "Hawaiian", price: 7 },
-    ],
-  };
-}
+  console.log(`
+    Radius : ${radius}
+    Diameter : ${diameter}
+    Circumference : ${circumference}
+    Area : ${area}`);
+};
+circleProperties(5);
 
-let menu = initMenu();
-let cart: Cart = { items: [] };
-
-let choosenPizza = menu.pizzas.find((pizza) => pizza.name === "Margherita");
-if (!choosenPizza) {
-  throw new Error("Pizza not found");
-}
-
-let choosenPizza2 = menu.pizzas.find((pizza) => pizza.name === "Pepperoni");
-if (!choosenPizza2) {
-  throw new Error("Pizza not found");
-}
-
-let choosenPizza3 = menu.pizzas.find((pizza) => pizza.name === "Hawaiian");
-if (!choosenPizza3) {
-  throw new Error("Pizza not found");
-}
-
-cart.items.push({ pizza: choosenPizza, quantity: 1 });
-cart.items.push({ pizza: choosenPizza2, quantity: 6 });
-cart.items.push({ pizza: choosenPizza3, quantity: 3 });
-
-console.log(JSON.stringify(cart, null, 2));
-
-let order: Order = {
-  cart: cart,
-  total: cart.items.reduce(
-    (acc, item) => acc + item.pizza.price * item.quantity,
-    0
-  ),
+/*
+  3. Write a code to find angles of triangle if two angles are given
+  Input: a = 80, b = 65
+  Output: 35
+*/
+const findThirdAngles = (a: number, b: number): number => {
+  return 180 - (a + b);
 };
 
-// Object destructuring
-const {
-  total,
-  cart: { items },
-} = order;
+const a = 80;
+const b = 65;
+const result = findThirdAngles(a, b);
+console.log(`\nThid angles of triangle : ${result}`);
 
-// Array destructuring
-// const firstItem = items[0];
-// const restItems = items.slice(1);
-const [firstItem, secondItem, ...restItems] = items;
+/*
+  4. Write a code to get difference between dates in days.
+  Input: date1 = 2024-03-19, date2 = 2024-03-21
+  Output: 2
+*/
+const getDaysDifference = (date1: string, date2: string): number => {
+  // create a new date object based on user input
+  const dateOne = new Date(date1);
+  const dateTwo = new Date(date2);
 
-// Spread operator
-const itemsClone = [
-  ...items,
-  { pizza: { name: "Margherita2", price: 5 }, quantity: 1 },
-];
+  // get time difference
+  const timeDifference = dateTwo.getTime() - dateOne.getTime();
 
-console.log(JSON.stringify(order, null, 2));
+  // convert time difference (in milisecond) to days
+  const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+  return daysDifference;
+};
+
+const date1 = "2024-03-19";
+const date2 = "2024-03-21";
+const daysDifference: number = getDaysDifference(date1, date2);
+console.log(
+  `Days difference between ${date1} and ${date2} : ${daysDifference} days`
+);
+
+/*
+  5. Write a code to print your name initial in uppercase
+  Input: John Doe
+  Output: JD
+*/
+const printInitialName = (fullName: string): string => {
+  const nameArray: string[] = fullName.split(" ");
+
+  // for debugging only will delete later
+  console.log(nameArray);
+
+  // get each index value first character
+  const getInitials = nameArray
+    .map((name) => name.charAt(0).toUpperCase())
+    .join("");
+
+  return getInitials;
+};
+
+// normal function
+function printInitial(fullName: string): string {
+  const nameArray: string[] = fullName.split(" ");
+
+  // for debugging only will delete later
+  console.log(nameArray);
+
+  // get each index value first character
+  const getInitials = nameArray
+    .map((name) => name.charAt(0).toUpperCase())
+    .join("");
+
+  return getInitials;
+}
+
+const fullName: string = "Fernaldy Saputra";
+const initialName: string = printInitialName(fullName);
+console.log(`Full name : ${fullName}`);
+console.log(`Initial name : ${initialName}`);
